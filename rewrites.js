7e30a88cac319da6d0e3c54911148387938ff315
brian wilkerson <bwilkerson@mudynamics.com>
2012-06-08 10:27:48 -0700

    rewrites now working. This would be a common rewrite pattern for any couchapp. Unclear why other blog posters did not include rules for db/:id, since the app won't work without it.

--- rewrites.js
+++ rewrites.js
@@ -2,14 +2,26 @@
  * this file contains the json representation for rewrite rules
 **/
 [
-  { // rewriting / to index.html
-    "from":"/",
-    "to":"index.html",
-    "method":"GET",
-    "query":{}
-  },
-  { // keeping relative urls sane
-    "from":"/*",
-    "to":"/*"
-  }
+{ // rewriting / to index.html
+  "from":"/",
+  "to":"index.html",
+  "method":"GET",
+  "query":{}
+},
+{ // this makes backbone-couchdb _views work properly
+  "from": "/ghostwriter/_design/ghostwriter/*",
+  "to": "/*"
+},
+{ // this is for GET, PUT, DELETE of existing models
+  "from": "/ghostwriter/:id",
+  "to": "../../:id"
+},
+{ // this handles POST for a new model
+  "from": "/ghostwriter/",
+  "to": "../../"
+},
+{ // all other couchapp attachments
+  "from" : "/*",
+  "to": "/*"
+}
 ]
